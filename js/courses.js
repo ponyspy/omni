a21c9117c81a2bf7c975f90aa69f4a50fa4d6d87
ponyspy <desade4me@gmail.com>
2017-01-14 21:11:53 +0300

    this

--- js/courses.js
+++ js/courses.js
@@ -1,40 +1,55 @@
 $(document).ready(function() {
-	$(document).on('click', '.course_block', function() {
-		$(this).data('clicked', !$(this).data('clicked'));
 
-		if ($(this).data('clicked')) {
-			$(this).children('.course_preview_lessons').addClass('hide')
-						 .end().children('.course_lessons_block').slideDown(300);
+	$(document).on('click', '.course_block', function(e) {
+		var $this = $(this);
+		$this.data('clicked', !$this.data('clicked'));
+
+		if ($this.data('clicked')) {
+			$this.children('.course_preview_lessons').addClass('hide').end()
+					 .children('.course_lessons_block').slideDown(300);
 		} else {
-			$(this).children('.course_preview_lessons').removeClass('hide')
-						 .end().children('.course_lessons_block').slideUp(300);
+			$this.children('.course_preview_lessons').removeClass('hide').end()
+					 .children('.course_lessons_block').slideUp(300);
 		}
 	});
 
-	$(document).on('click', '.section_description', function() {
-		$(this).data('clicked', !$(this).data('clicked'));
+	$(document).on('click', '.section_description', function(e) {
+		var $this = $(this);
+		$this.data('clicked', !$this.data('clicked'));
 
-		if ($(this).data('clicked')) {
-			$(this).parent('.section_block').children('.section_body').slideDown(300);
+		if ($this.data('clicked')) {
+			$this.parent('.section_block').children('.section_body').slideDown(300);
 		} else {
-			$(this).parent('.section_block').children('.section_body').slideUp(300);
+			$this.parent('.section_block').children('.section_body').slideUp(300);
 		}
 	});
 
-	$('.menu_item').on('click', function() {
-		var current = $(this).attr('class').split(' ')[1];
+	// $(document).on('click', '.lang', function(e) {
+	// 	var $this = $(this);
+	// 	$this.data('clicked', !$this.data('clicked'));
 
-		if ($(this).hasClass('open')) {
-			$(this).removeClass('open');
+	// 	if ($this.data('clicked')) {
+
+	// 	} else {
+
+	// 	}
+	// });
+
+	$('.menu_item').on('click', function(e) {
+		var $this = $(this);
+		var current = $this.attr('class').split(' ')[1];
+
+		if ($this.hasClass('open')) {
+			$this.removeClass('open');
 			$('.panel.' + current).removeClass('open');
 		} else {
-			var index = $(this).index('.menu_item');
+			var index = $this.index('.menu_item');
 			$('.menu_item').removeClass('open').eq(index).addClass('open');
 			$('.panel').removeClass('open').filter('.' + current).addClass('open');
 		}
 	});
 
-	$('.menu_navigate').on('click', function() {
+	$('.menu_navigate').on('click', function(e) {
 		$current_course = $('.course_block').eq(0);
 
 		$('.column.left').animate({'scrollTop': $current_course.position().top}, 300, function() {
@@ -44,7 +59,7 @@ $(document).ready(function() {
 		});
 	});
 
-	$('.logo').on('click', function() {
+	$('.logo').on('click', function(e) {
 		$('.column_inner').fadeToggle(300);
 	});
 
